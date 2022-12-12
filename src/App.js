1be95961c1d662e21e29a4b348945a8a09e883ab
QWOFTQ <rkdcofls3890@naver.com>
2022-12-12 15:48:59 +0900

    cal-real

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import Calendar from './components/Calendar'
+import AddNote from './components/AddNote'
+import UpdateNote from './components/UpdateNote'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Notes from './components/Notes'
+import { Col, Container, Row } from 'react-bootstrap'
+import GoogleLogin from './components/Google'
 
 function App() {
+  const showUpdateNote = useSelector((state) => state.note.showUpdateNote)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <body>
+      <>
+        <header>
+          <h1>MENU</h1>
+          <nav>
+            <span>Menu1</span>
+            <span>Menu2</span>
+            <span>Menu3</span>
+          </nav>
+        </header>
+        <div className="App">
+          <Container>
+            <Row>
+              <Col xs={40} md={50}>
+                <Calendar />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} md={6}>
+                <Notes />
+              </Col>
+              <Col xs={12} md={6}>
+                <AddNote />
+              </Col>
+              <Col xs={12} md={6}>
+                {showUpdateNote && <UpdateNote />}
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="App">
+          <GoogleLogin
+            success={(res) => console.log(res)}
+            fail={(res) => console.log(res)}
+          />
+        </div>
+        <footer>
+          Copyright &copy; 2020 by Sumit Jangir. All Rights Reserved.
+        </footer>
+      </>
+    </body>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { reducer } from "../src/reducers";
+import { createStore } from "redux";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
+import { formatDate } from "../src/common";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const persistedNotes = localStorage.getItem("notes")
+  ? JSON.parse(localStorage.getItem("notes"))
+  : [];
+
+const store = createStore(reducer, {
+  note: { notes: persistedNotes, date: formatDate(new Date()) },
+  date: new Date(),
+});
+
+store.subscribe(() => {
+  localStorage.setItem("notes", JSON.stringify(store.getState().note.notes));
+});
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
